14e9a656f9e3a7a4e49ffabe4c2a0122c952c959
Francesco <bembofrancesco@icloud.com>
2021-03-25 01:03:55 +1100

    Importing HEX colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 // ignore: unused_import
 import 'dart:html';
-
+//import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter/material.dart';
 import 'package:kiresu_web/Screens/login_screen.dart';
 
 void main() {
   runApp(KiresuManagerApp());
 }
+
+Color myHexColor = Color(0xffD24848);
 
 class KiresuManagerApp extends StatelessWidget {
   @override
